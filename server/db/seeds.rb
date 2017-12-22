feb97e2c72b03a2c60001ebc19459ab10323403c
tokikaze0604 <tokikaze0604@gmail.com>
2017-12-22 20:42:41 +0900

    seedをふぁぼ挿入用に変更

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -5,14 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'time'
+
 ActiveRecord::Base.transaction do
-  Favorite.delete_all
-  10.times do |idx|
-    Favorite.create!(
-        tweet_id: "#{idx}",
-        text: "本文 #{idx}",
-        user_name: "user_name #{idx}",
-        created_at: Date.today.to_time
+  file_path = 'public/favorites.txt'
+  json_data = open(file_path) do |io|
+    JSON.load(io)
+  end
+
+  json_data.each do |j|
+    Favorite.create(
+      tweet_id: j[1]['tweet_id'],
+      text: j[1]['text'],
+      user_name: j[1]['user_name'],
+      created_at: Time.parse(j[1]['created_at'])
     )
   end
 end
