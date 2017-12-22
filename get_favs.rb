00ee58a0afe3666cad522e34cf590d42e6c1c3b8
tokikaze0604 <tokikaze0604@gmail.com>
2017-12-22 20:21:11 +0900

    ファイルの中身を整理

--- get_favs.rb
+++ get_favs.rb
@@ -22,13 +22,6 @@ def get_favs(n = 200, max_id)
   @client.favorites(:count => n, :max_id => max_id)
 end
 
-@client = Twitter::REST::Client.new do |config|
-  config.consumer_key = ENV['CONSUMER_KEY']
-  config.consumer_secret = ENV['CONSUMER_SECRET']
-  config.access_token = ENV['ACCESS_TOKEN']
-  config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
-end
-
 def append_fav(favs, fav, count)
   data = {
     "tweet_id" => fav.id,
@@ -37,7 +30,14 @@ def append_fav(favs, fav, count)
     "created_at" => fav.created_at
   }
 
-  tweets.store(count.to_s, data)
+  favs.store(count.to_s, data)
+end
+
+@client = Twitter::REST::Client.new do |config|
+  config.consumer_key = ENV['CONSUMER_KEY']
+  config.consumer_secret = ENV['CONSUMER_SECRET']
+  config.access_token = ENV['ACCESS_TOKEN']
+  config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
 end
 
 max_id = get_recent_fav_id() # 最新のいいねから取得開始
@@ -53,7 +53,7 @@ while true do
     res = get_favs(max_id) # max_id以下のidのふぁぼ（ツイート）が取得できる
 
     if res.empty? then # 何も帰ってこなかったら遡れる限界?
-      file.puts tweets.to_json
+      file.puts favs.to_json
       puts '終わったよ'
       break
     end
